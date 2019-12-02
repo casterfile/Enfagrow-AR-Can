0d49288f9844a946541d87ba1746375d2f9c1920
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-02 11:16:41 +0800

    Add all my files

---
+++ Assets/_MyAsset/_Script/Scene01Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene01Controller : MonoBehaviour {
+	public GameObject Message;
+	// Use this for initialization
+	void Start () {
+		Message.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	public void ShowMessage () {
+		Message.SetActive(true);
+	}
+
+	public void HideMessage () {
+		Message.SetActive(false);
+	}
+}
